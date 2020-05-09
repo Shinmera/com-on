9ddf896531111bb2a9364a773c76e557e14aafa6
Shinmera <shinmera@tymoon.eu>
2020-05-09 17:30:28 +0200

    Fix GUIDs to actually work as intended (hopefully)

--- guid.lisp
+++ guid.lisp
@@ -6,54 +6,102 @@
 
 (in-package #:org.shirakumo.com-on)
 
-(defclass guid ()
-  ((bytes :initform (make-array 16 :element-type '(unsigned-byte 8)) :reader bytes)))
+;;;; Note:
+;; We define the actual type here to be a pointer in order to allow
+;; defining arguments and return values of functions to work properly
+;; without complaining about libffi, as usually GUIDs are in fact
+;; passed by pointer and not by value (thank god).
+;;
+;; However, doing so means that when we want to allocate a GUID
+;; object, we would be allocating only enough space for a pointer,
+;; which is not going to be enough on any current platform. So we
+;; have to override FOREIGN-TYPE-SIZE as well to fake being a
+;; structure.
+(cffi:define-foreign-type guid ()
+  ((bytes :initform (make-array 16 :element-type '(unsigned-byte 8)) :initarg :bytes :reader bytes))
+  (:actual-type :pointer))
 
-(defmethod initialize-instance :after ((guid guid) &key id)
+(cffi:define-parse-method guid ()
+  (make-instance 'guid :bytes NIL))
+
+(defmethod cffi:foreign-type-size ((type guid))
+  (cffi:foreign-type-size 'com:guid))
+
+(defmethod cffi:translate-to-foreign ((guid guid) (type guid))
+  (cffi:translate-into-foreign-memory guid type (cffi:foreign-alloc :uint8 :count 16)))
+
+(defmethod cffi:translate-from-foreign (ptr (type guid))
+  (make-instance 'guid :id ptr))
+
+(defmethod cffi:free-translated-object (ptr (type guid) param)
+  (declare (ignore param))
+  (cffi:foreign-free ptr))
+
+(defmethod cffi:translate-into-foreign-memory ((guid guid) (type guid) ptr)
   (let ((dat (bytes guid)))
-    (etypecase id
-      (string
-       (let ((i 0))
-         (flet ((read-bytes (start end mode)
-                  (let ((int (parse-integer id :start start :end end :radix 16))
-                        (cnt (/ (- end start) 2)))
-                    (loop for j from 0 below cnt
-                          for byte = (ecase mode
-                                       (:lsb (ldb (byte 8 (* 8 j)) int))
-                                       (:msb (ldb (byte 8 (* 8 (- cnt j 1))) int)))
-                          do (setf (aref dat i) byte)
-                             (incf i)))))
-           (read-bytes 0 8 :lsb)
-           (read-bytes 9 13 :lsb)
-           (read-bytes 14 18 :lsb)
-           (read-bytes 19 23 :msb)
-           (read-bytes 24 36 :msb))))
-      (cons
-       (ecase (length id)
-         (16 (replace dat id))
-         (11 (destructuring-bind (d1 d2 d3 &rest d4) id
-               (setf (aref dat 0) (ldb (byte 8 0) d1))
-               (setf (aref dat 1) (ldb (byte 8 8) d1))
-               (setf (aref dat 2) (ldb (byte 8 16) d1))
-               (setf (aref dat 3) (ldb (byte 8 24) d1))
-               (setf (aref dat 4) (ldb (byte 8 0) d2))
-               (setf (aref dat 5) (ldb (byte 8 8) d2))
-               (setf (aref dat 6) (ldb (byte 8 0) d3))
-               (setf (aref dat 7) (ldb (byte 8 8) d3))
-               (loop for i from 8 below 16
-                     for byte in d4
-                     do (setf (aref dat i) byte))))))
-      (cffi:foreign-pointer
-       (loop with dat = dat
-             for i from 0 below 16
-             do (setf (aref dat i) (cffi:mem-aref id :uint8 i))))
-      (vector
-       (replace dat id))
-      (null
-       (fill dat 0)))))
+    (dotimes (i 16 ptr)
+      (setf (cffi:mem-aref ptr :uint8 i) (aref dat i)))))
+
+(defmethod cffi:expand-from-foreign (ptr (type guid))
+  `(make-instance 'guid :id ,ptr))
+
+(defmethod cffi:expand-to-foreign-dyn (val var body (type guid))
+  (let ((dat (gensym "DAT"))
+        (i (gensym "I")))
+    `(cffi:with-foreign-object (,var 'guid)
+       (let ((,dat (bytes ,val)))
+         (dotimes (,i 16)
+           (setf (cffi:mem-aref ,var :uint8 ,i) (aref ,dat ,i))))
+       ,@body)))
+
+(defmethod initialize-instance :after ((guid guid) &key (id NIL id-p))
+  (when id-p
+    (let ((dat (bytes guid)))
+      (etypecase id
+        (string
+         (let ((i 0))
+           (flet ((read-bytes (start end mode)
+                    (let ((int (parse-integer id :start start :end end :radix 16))
+                          (cnt (/ (- end start) 2)))
+                      (loop for j from 0 below cnt
+                            for byte = (ecase mode
+                                         (:lsb (ldb (byte 8 (* 8 j)) int))
+                                         (:msb (ldb (byte 8 (* 8 (- cnt j 1))) int)))
+                            do (setf (aref dat i) byte)
+                               (incf i)))))
+             (read-bytes 0 8 :lsb)
+             (read-bytes 9 13 :lsb)
+             (read-bytes 14 18 :lsb)
+             (read-bytes 19 23 :msb)
+             (read-bytes 24 36 :msb))))
+        (cons
+         (ecase (length id)
+           (16 (replace dat id))
+           (11 (destructuring-bind (d1 d2 d3 &rest d4) id
+                 (setf (aref dat 0) (ldb (byte 8 0) d1))
+                 (setf (aref dat 1) (ldb (byte 8 8) d1))
+                 (setf (aref dat 2) (ldb (byte 8 16) d1))
+                 (setf (aref dat 3) (ldb (byte 8 24) d1))
+                 (setf (aref dat 4) (ldb (byte 8 0) d2))
+                 (setf (aref dat 5) (ldb (byte 8 8) d2))
+                 (setf (aref dat 6) (ldb (byte 8 0) d3))
+                 (setf (aref dat 7) (ldb (byte 8 8) d3))
+                 (loop for i from 8 below 16
+                       for byte in d4
+                       do (setf (aref dat i) byte))))))
+        (cffi:foreign-pointer
+         (loop with dat = dat
+               for i from 0 below 16
+               do (setf (aref dat i) (cffi:mem-aref id :uint8 i))))
+        (vector
+         (replace dat id))
+        (null
+         (fill dat 0))))))
 
 (defmethod print-object ((guid guid) stream)
-  (format stream "(~s ~s)" 'guid (guid-string guid)))
+  (if (bytes guid)
+      (format stream "(~s ~s)" 'guid (guid-string guid))
+      (call-next-method)))
 
 (defmethod make-load-form ((guid guid) &optional env)
   (declare (ignore env))
@@ -82,38 +130,6 @@
   (loop for ab across (bytes a)
         for bb across (bytes b)
         always (= ab bb)))
-
-(cffi:defctype guid :pointer)
-
-(defmethod cffi:foreign-type-size ((type (eql 'guid)))
-  (cffi:foreign-type-size 'com:guid))
-
-(defmethod cffi:translate-to-foreign ((guid guid) (type (eql 'guid)))
-  (cffi:translate-to-foreign guid (find-class 'com:guid)))
-
-(defmethod cffi:translate-from-foreign (ptr (type (eql 'guid)))
-  (cffi:translate-from-foreign ptr (find-class 'com:guid)))
-
-(defmethod cffi:free-translated-object (ptr (type (eql 'guid)) param)
-  (cffi:free-translated-object ptr (find-class 'com:guid) param))
-
-(defmethod cffi:translate-into-foreign-memory ((guid guid) (type (eql 'guid)) ptr)
-  (cffi:translate-into-foreign-memory guid (find-class 'com:guid) ptr))
-
-(defmethod cffi:translate-to-foreign ((guid guid) (type com:guid))
-  (cffi:translate-into-foreign-memory guid type (cffi:foreign-alloc :uint8 :count 16)))
-
-(defmethod cffi:translate-from-foreign (ptr (type com:guid))
-  (make-instance 'guid :id ptr))
-
-(defmethod cffi:free-translated-object (ptr (type com:guid) param)
-  (declare (ignore param))
-  (cffi:foreign-free ptr))
-
-(defmethod cffi:translate-into-foreign-memory ((guid guid) (type com:guid) ptr)
-  (let ((dat (bytes guid)))
-    (dotimes (i 16 ptr)
-      (setf (cffi:mem-aref ptr :uint8 i) (aref dat i)))))
 
 (defun guid (&rest id)
   (make-instance 'guid :id (if (cdr id) id (first id))))
